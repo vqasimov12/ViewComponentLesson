4ab32d306df33c65d9a2d915abf614c60c462296
vqasimov12 <q.vaqif1211@gmail.com>
2025-01-27 01:11:34 +0400

    Razor pages

--- ProductTable.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE Products(
-Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-[Name] NVARCHAR(50) NOT NULL,
-[Price] Money NOT NULL
-)
-GO
-INSERT INTO Products([Name],[Price])
-VALUES('Apple',1200),
-('Samsung' , 1155)
